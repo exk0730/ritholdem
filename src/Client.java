5d8360a20aa88c72434acd039f41faf0c8f01281
emilien.girault <emilien.girault@52aefe58-c70b-11dd-a2c4-6dc3ad32ce87>
2009-01-15 22:38:53 +0000

    RMI works! Implemented register() into the server; all other methods are empty. Ba sure to run rmiregistry in the classes folder of Netbeans before running the server.

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,6 @@
 /**
- * RMI Interface for the server
- * @author Eric Kisner, Emilien Girault
+ * Client
+ * @author Emilien Girault
  * @date 1/12/09
  */
 
@@ -13,17 +13,40 @@ public class Client {
      */
     public static String SERVER_NAME = "BlackJackServer";
 
+    /**
+     * Server RMI object
+     * TODO: Will be private
+     */
+    public RMIInterface server;
 
+    public Client(){
+        try {
+            server = (RMIInterface) Naming.lookup(SERVER_NAME);
+        } catch(Exception e) {
+            System.out.println("The client cannot resolve the server. Did you launch it?");
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Main program
+     * @param args
+     */
     public static void main(String[] args){
 
-        System.out.println("coucou");
+        /**
+         * This is a test program for deliverable 2
+         * It only call some methods to get data from and to the database.
+         */
+        Client client = new Client();
+        
 
         try {
-
-        RMIInterface server = (RMIInterface) Naming.lookup(SERVER_NAME);
+            System.out.println("register = " + client.server.register("emilien", "p4ss", "Emilien", "Girault", "bla@bla.com", "1456", 8));
+            System.out.println("lll");
 
         } catch(Exception e){
-            System.out.println("Client error: "+e.getMessage());
+            System.out.println("The client encountered an error. The message is: " + e.getMessage());
             e.printStackTrace();
         }
     }
