fd951d55ba70a8698aa479d7638e4df6f46c8b09
emilien.girault <emilien.girault@52aefe58-c70b-11dd-a2c4-6dc3ad32ce87>
2009-01-15 05:22:03 +0000

    Added a register() method, handled PreparedStatements properly, cleaned the code

--- trunk/src/Server.java
+++ trunk/src/Server.java
@@ -11,24 +11,26 @@ public class Server {
     /**
     * Database
     */
-    Data data;
+    public Data data; //TODO change into private
 
     /**
-    * Unique instance (Singleton Design Pattern)
-    */
+     * Unique instance (Singleton Design Pattern)
+     */
     private static Server instance = null;
 
     /**
-    * Private constructor (use instance() instead)
-    */
+     * Private constructor (use instance() instead)
+     * @throws java.sql.SQLException
+     */
     private Server() throws SQLException {
         data = Data.instance();
     }
 
     /**
-    * Return the unique instance of the class (Singleton Design Pattern)
-    * @return Server instance
-    */
+     * Return the unique instance of the class (Singleton Design Pattern)
+     * @return Server instance
+     * @throws java.sql.SQLException
+     */
     public static Server instance() throws SQLException {
         if(instance == null){
           instance = new Server();
@@ -36,9 +38,16 @@ public class Server {
         return instance;
     }
 
+    /**
+     * The main program
+     * @param args arguments
+     */
     public static void main(String args[]){
         try {
             Server s = instance();
+            //System.out.println("name = " + s.data.getMaxUserID());
+            System.out.println("register = " + s.data.register("eric", "coucou", "moi", "lui"));
+
         } catch(SQLException e){
             System.out.println("Error: "+e.getMessage());
         }
